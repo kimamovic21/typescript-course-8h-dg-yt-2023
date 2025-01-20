fdde227880f5af671ebd2403c8a47948f5299c7f
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-20 11:05:54 +0100

    Lesson 15

---
+++ lesson15/src/App.tsx
@@ -0,0 +1,16 @@
+import { CounterProvider } from './context/CounterContext';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <>
+      <CounterProvider>
+        <Counter>
+          {(num: number) => <>Current Count: {num}</>}
+        </Counter>
+      </CounterProvider>
+    </>
+  );
+};
+
+export default App;
